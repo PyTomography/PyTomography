0e3db81e2d5afd80e7b3a9b1835d26eff8934942
lukepolson <ubuntu@ip-10-0-4-206.ca-central-1.compute.internal>
2023-01-25 01:23:07 +0000

    added alpha-level support for opening SIMIND output files. Will still need to do a lot of debugging

--- src/pytomography/classes.py
+++ src/pytomography/classes.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from torchvision.transforms.functional import rotate
 from torchvision.transforms import InterpolationMode
 import numpy as np
+import os
+from pathlib import Path
 
 
 # cumulative sum, but initial voxel only contriubtes 1/2 mu dx
@@ -53,6 +55,76 @@ def get_PSF_transform(sigma, kernel_size, delta=1e-9, device='cpu', kernel_dimen
     layer.weight.data = kernel.unsqueeze(dim=1).to(device)
     return layer
 
+def find_first_entry_containing_substring(list_of_attributes, substring, dtype=np.float32):
+    line = list_of_attributes[np.char.find(list_of_attributes, substring)>=0][0]
+    if dtype == np.float32:
+        return np.float32(line.replace('\n', '').split(':=')[-1])
+    elif dtype == str:
+        return (line.replace('\n', '').split(':=')[-1].replace(' ', ''))
+    elif dtype == int:
+        return int(line.replace('\n', '').split(':=')[-1].replace(' ', ''))
+
+def simind_projections_to_data(headerfile):    
+    with open(headerfile) as f:
+        headerdata = f.readlines()
+    headerdata = np.array(headerdata)
+    num_proj = find_first_entry_containing_substring(headerdata, 'total number of images', int)
+    proj_dim1 = find_first_entry_containing_substring(headerdata, 'matrix size [1]', int)
+    proj_dim2 = find_first_entry_containing_substring(headerdata, 'matrix size [2]', int)
+    dx = find_first_entry_containing_substring(headerdata, 'scaling factor (mm/pixel) [1]', np.float32) / 10 # to mm
+    number_format = find_first_entry_containing_substring(headerdata, 'number format', str)
+    num_bytes_per_pixel = find_first_entry_containing_substring(headerdata, 'number of bytes per pixel', np.float32)
+    extent_of_rotation = find_first_entry_containing_substring(headerdata, 'extent of rotation', np.float32)
+    number_of_projections = find_first_entry_containing_substring(headerdata, 'number of projections', int)
+    start_angle = find_first_entry_containing_substring(headerdata, 'start angle', np.float32)
+    angles = np.linspace(start_angle, extent_of_rotation, number_of_projections, endpoint=False)
+    radius = find_first_entry_containing_substring(headerdata, 'Radius', np.float32)
+    imagefile = find_first_entry_containing_substring(headerdata, 'name of data file', str)
+    shape_proj= (num_proj, proj_dim1, proj_dim2)
+    shape_obj = (proj_dim1, proj_dim1, proj_dim2)
+    object_meta = ObjectMeta(dx, shape_obj)
+    image_meta = ImageMeta(object_meta, angles, np.ones(len(angles))*radius)
+    projections = np.fromfile(os.path.join(str(Path(headerfile).parent), imagefile), dtype=np.float32)
+    projections = np.transpose(projections.reshape((num_proj,proj_dim2,proj_dim1))[:,::-1], (0,2,1))
+    projections = torch.tensor(projections.copy()).unsqueeze(dim=0)
+    return object_meta, image_meta, projections
+
+def simind_CT_to_data(headerfile):    
+    with open(headerfile) as f:
+        headerdata = f.readlines()
+    headerdata = np.array(headerdata)
+    matrix_size_1 = find_first_entry_containing_substring(headerdata, 'matrix size [1]', int)
+    matrix_size_2 = find_first_entry_containing_substring(headerdata, 'matrix size [2]', int)
+    matrix_size_3 = find_first_entry_containing_substring(headerdata, 'matrix size [3]', int)
+    shape = (matrix_size_1, matrix_size_2, matrix_size_3)
+    imagefile = find_first_entry_containing_substring(headerdata, 'name of data file', str)
+    CT = np.fromfile(os.path.join(str(Path(headerfile).parent), imagefile), dtype=np.float32)
+    CT = np.transpose(CT.reshape(shape)[::-1,::-1], (2,1,0))
+    CT = torch.tensor(CT.copy())
+    return CT
+
+def get_osem_net(projections_header, object_initial='ones', CT_header=None, collimator_slope=None, collimator_intercept=None, device='cpu'):
+    object_meta, image_meta, projections = simind_projections_to_data(projections_header)
+    object_correction_nets = []
+    image_correction_nets = []
+    if CT_header:
+        CT = simind_CT_to_data(CT_header)
+        CT_net = CTCorrectionNet(object_meta, image_meta, CT.unsqueeze(dim=0).to(device), device=device)
+        object_correction_nets.append(CT_net)
+        # fill this in later
+    if collimator_slope:
+        []
+        # fill this in later
+    fp_net = ForwardProjectionNet(object_correction_nets, image_correction_nets,
+                                object_meta, image_meta, device=device)
+    bp_net = BackProjectionNet(object_correction_nets, image_correction_nets,
+                                object_meta, image_meta, device=device)
+    if object_initial == 'ones':
+        object_initial_array = torch.ones(object_meta.shape).unsqueeze(dim=0).to(device)
+    osem_net = OSEMNet(object_initial_array, fp_net, bp_net)
+    osem_net.set_image(projections.to(device))
+    return osem_net
+
 
 class ObjectMeta():
     def __init__(self, dx, shape):
@@ -68,12 +140,7 @@ class ImageMeta():
         self.shape = (self.num_projections, object_meta.shape[1], object_meta.shape[2])
         
 
-# INPUT: 
-# object [batch_size, Lx, Ly, Lz] 
-# CT [batch_size, Lx, Ly, Lz]
-# angle
-# OUTPUT: 
-# CT corrected object [batch_size, Lx, Ly, Lz]
+# TODO: Option to store all the matrices in memory (120*128*128*128 * 32bytes = 8 Gbytes)
 
 class CTCorrectionNet(nn.Module):
     '''Apply attenuation correction to an object.
@@ -82,7 +149,7 @@ class CTCorrectionNet(nn.Module):
     using an array of CT values such that subsequent projection
     yields an appropriate image
     '''
-    def __init__(self, object_meta, image_meta, CT, device='cpu'):
+    def __init__(self, object_meta, image_meta, CT, store_in_memory=False, device='cpu'):
         '''
         Parameters
         --------
@@ -100,6 +167,11 @@ class CTCorrectionNet(nn.Module):
         self.object_meta = object_meta
         self.image_meta = image_meta
         self.device = device
+        self.store_in_memory = store_in_memory
+        if self.store_in_memory:
+            self.probability_matrices = []
+            for i, angle in enumerate(self.image_meta.angles):
+                self.probability_matrices.append(get_prob_of_detection_matrix(rotate_detector_z(self.CT, angle), self.object_meta.dx).to(self.device))
     @torch.no_grad()
     def forward(self, object_i, i, norm_constant=None):
         '''Modify object using attenuation correction.
@@ -119,7 +191,10 @@ class CTCorrectionNet(nn.Module):
         Returns:
           Pytorch tensor of size [batch_size, Lx, Ly, Lz] cor
         '''
-        norm_factor = get_prob_of_detection_matrix(rotate_detector_z(self.CT, self.image_meta.angles[i]), self.object_meta.dx).to(self.device)
+        if self.store_in_memory:
+            norm_factor = self.probability_matrices[i]
+        else:
+            norm_factor = get_prob_of_detection_matrix(rotate_detector_z(self.CT, self.image_meta.angles[i]), self.object_meta.dx).to(self.device)
         if norm_constant is not None:
             norm_constant*=norm_factor
         return object_i*norm_factor
@@ -196,7 +271,7 @@ class BackProjectionNet(nn.Module):
         self.object_meta = object_meta
         self.image_meta = image_meta
 
-    def forward(self, image, angle_subset=None, delta=1e-11):
+    def forward(self, image, angle_subset=None, prior=None, delta=1e-11):
         N_angles = self.image_meta.num_projections
         object = torch.zeros([image.shape[0], *self.object_meta.shape]).to(self.device)
         norm_constant = torch.zeros([image.shape[0], *self.object_meta.shape]).to(self.device)
@@ -208,17 +283,21 @@ class BackProjectionNet(nn.Module):
                 object_i = net(object_i, i, norm_constant=norm_constant_i)
             norm_constant += rotate_detector_z(norm_constant_i, self.image_meta.angles[i], negative=True)
             object += rotate_detector_z(object_i, self.image_meta.angles[i], negative=True)
+        if prior:
+            norm_constant += prior()
         return object/(norm_constant + delta)
 
 class OSEMNet(nn.Module):
     def __init__(self, 
                  object_initial,
                  forward_projection_net,
-                 back_projection_net):
+                 back_projection_net,
+                 prior = None):
         super(OSEMNet, self).__init__()
         self.forward_projection_net = forward_projection_net
         self.back_projection_net = back_projection_net
         self.object_prediction = object_initial
+        self.prior = prior
 
     def get_subset_splits(self, n_subsets, n_angles):
         indices = np.arange(n_angles).astype(int)
@@ -227,24 +306,56 @@ class OSEMNet(nn.Module):
             subset_indices_array.append(indices[i::n_subsets])
         return subset_indices_array
 
-    def forward(self, image, n_iters, n_subsets, comparisons=None, delta=1e-11):
-        subset_indices_array = self.get_subset_splits(n_subsets, image.shape[1])
+    def set_image(self, image):
+        self.image = image
+
+    def forward(self, n_iters, n_subsets, comparisons=None, delta=1e-11):
+        subset_indices_array = self.get_subset_splits(n_subsets, self.image.shape[1])
         for j in range(n_iters):
             for subset_indices in subset_indices_array:
-                ratio = image / (self.forward_projection_net(self.object_prediction, angle_subset=subset_indices) + delta)
-                self.object_prediction = self.object_prediction * self.back_projection_net(ratio, angle_subset=subset_indices)
+                # Set OSL Prior to have object from previous prediction
+                if self.prior:
+                    self.prior.set_object(torch.clone(self.object_prediction))
+                ratio = self.image / (self.forward_projection_net(self.object_prediction, angle_subset=subset_indices) + delta)
+                self.object_prediction = self.object_prediction * self.back_projection_net(ratio, angle_subset=subset_indices, prior=self.prior)
                 if comparisons:
                     for key in comparisons.keys():
                         comparisons[key].compare(self.object_prediction)
         return self.object_prediction
 
+class QuadraticPriorOSL(nn.Module):
+    def __init__(self, beta, device='cpu'):
+        super(QuadraticPriorOSL, self).__init__()
+        self.beta = beta
+        self.device=device
+        self.kernel = self.get_kernel()
+    def get_kernel(self):
+        kern = torch.nn.Conv3d(1, 1, 3, padding='same', padding_mode='reflect', bias=0, device=self.device)
+        xv, yv, zv = torch.meshgrid(*3*[torch.tensor([-1,0,1])])
+        d = torch.sqrt(xv**2+yv**2+zv**2)
+        d[1][1][1] = 1 #little hack that prevents division by zero
+        dinv = 1/d
+        dinv[1][1][1] = 0 # ^^
+        kern.weight.data[0][0] = dinv
+        return kern
+    def set_object(self, object):
+        self.object = object
+    def forward(self):
+        diff = self.kernel.weight.data.sum()*self.object.unsqueeze(dim=1) \
+                            - self.kernel(self.object.unsqueeze(dim=1)) 
+        return self.beta*diff[:,0]
+
 # RENAME ALL THIS LATER
 class CompareToNumber():
-    def __init__(self, number, mask):
+    def __init__(self, number, mask, norm_factor=None):
         self.number = number
         self.mask = mask
         self.biass = []
         self.vars = []
+        self.norm_factor = norm_factor
     def compare(self, prediction):
+        prediction = torch.clone(prediction)
+        if self.norm_factor:
+            prediction *= self.norm_factor 
         self.biass.append(torch.mean(prediction[self.mask] - self.number).item())
         self.vars.append(torch.var(prediction[self.mask] - self.number).item())
