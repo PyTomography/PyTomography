35e21b2e7410a2948ce48553f32ae1e0ea4769ae
Ubuntu <gpuvmadm@gpuvm00004jhubvm01.hwm40kusnnnubngo5w32f2kxsg.ux.internal.cloudapp.net>
2023-05-10 17:58:09 +0000

    change datafile names for SPECT collimation

--- src/pytomography/io/SPECT/dicom.py
+++ src/pytomography/io/SPECT/dicom.py
@@ -97,7 +97,18 @@ def get_scatter_from_TEW(
     index_peak: int,
     index_lower: int,
     index_upper: int
-    ):
+    ) -> torch.Tensor:
+    """Gets scatter estimate using the triple energy window method
+
+    Args:
+        file (str): Filepath of the DICOM file
+        index_peak (int): Index of the ``EnergyWindowInformationSequence`` DICOM attribute corresponding to the photopeak.
+        index_lower (int): Index of the ``EnergyWindowInformationSequence`` DICOM attribute corresponding to lower scatter window.
+        index_upper (int): Index of the ``EnergyWindowInformationSequence`` DICOM attribute corresponding to upper scatter window.
+
+    Returns:
+        torch.Tensor[1,Ltheta,Lr,Lz]: Tensor corresponding to the scatter estimate.
+    """
     ds = pydicom.read_file(file)
     ww_peak = get_window_width(ds, index_peak)
     ww_lower = get_window_width(ds, index_lower)
@@ -119,12 +130,26 @@ def get_attenuation_map_from_file(file_AM: str) -> torch.Tensor:
     attenuation_map =  ds.pixel_array / ds[0x033,0x1038].value
     return torch.tensor(np.transpose(attenuation_map, (2,1,0))).unsqueeze(dim=0)
 
-def get_psfmeta_from_scanner_params(camera_model, collimator_name, energy_keV):
+def get_psfmeta_from_scanner_params(
+    camera_model: str,
+    collimator_name: str,
+    energy_keV: float
+    ) -> PSFMeta:
+    """Obtains PSF metadata from SPECT camera/collimator parameters
+
+    Args:
+        camera_model (str): Name of SPECT camera. 
+        collimator_name (str): Name of collimator used.
+        energy_keV (float): Energy of the photopeak
+
+    Returns:
+        PSFMeta: PSF metadata.
+    """
 
     module_path = os.path.dirname(os.path.abspath(__file__))
     
-    scanner_datasheet = np.genfromtxt(os.path.join(module_path, '../../data/data_sheet.csv'), skip_header=1, dtype=['U50,U50,float,float'], delimiter=',', unpack=True)
-    attenuation_coefficient_energy = np.genfromtxt(os.path.join(module_path, '../../data/attenuation_values.csv'), skip_header = 1, dtype=['float,float'], delimiter=',', unpack = True)
+    scanner_datasheet = np.genfromtxt(os.path.join(module_path, '../../data/SPECT_collimator_parameters.csv'), skip_header=1, dtype=['U50,U50,float,float'], delimiter=',', unpack=True)
+    attenuation_coefficient_energy = np.genfromtxt(os.path.join(module_path, '../../data/lead_attenuation_values.csv'), skip_header = 1, dtype=['float,float'], delimiter=',', unpack = True)
         
     for i in range(len(scanner_datasheet)):
         if camera_model == scanner_datasheet[i][0] and collimator_name == scanner_datasheet[i][1]:
@@ -138,12 +163,11 @@ def get_psfmeta_from_scanner_params(camera_model, collimator_name, energy_keV):
     collimator_slope_FWHM = hole_diameter/(hole_length - (2/attenuation_coefficient))
     collimator_intercept_FWHM = hole_diameter
 
+    # convert to units of sigma
     collimator_slope = collimator_slope_FWHM * (1/(2*np.sqrt(2*np.log(2))))
     collimator_intercept = collimator_intercept_FWHM * (1/(2*np.sqrt(2*np.log(2))))
 
-    psf_meta = PSFMeta(collimator_slope, collimator_intercept)
-    
-    return (psf_meta)
+    return PSFMeta(collimator_slope, collimator_intercept)
 
 def get_attenuation_map_from_CT_slices(
     files_CT: Sequence[str],
