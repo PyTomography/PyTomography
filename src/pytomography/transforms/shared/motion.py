d75b3fbc478f10ebb4a69db62fee68bde9e6b457
lukepolson <lukepolson@outlook.com>
2023-12-11 16:30:51 +0000

    faster grid sampling in DVF Transform

--- src/pytomography/transforms/shared/motion.py
+++ src/pytomography/transforms/shared/motion.py
@@ -3,6 +3,7 @@ import torch
 import pytomography
 from pytomography.transforms import Transform
 from scipy.ndimage import map_coordinates
+from torch.nn.functional import grid_sample
 
 class DVFMotionTransform(Transform):
 	def __init__(
@@ -10,34 +11,85 @@ class DVFMotionTransform(Transform):
 		dvf_forward: torch.Tensor | None = None,
 		dvf_backward: torch.Tensor | None = None,
 		)-> None:
-		self.dvf_forward = dvf_forward
-		self.dvf_backward = dvf_backward
+		"""Object to object transform that uses a deformation vector field to deform an object. 
+
+		Args:
+			dvf_forward (torch.Tensor[Lx,Ly,Lz,3] | None, optional): Vector field correspond to forward transformation. If None, then no transformation is used. Defaults to None.
+			dvf_backward (torch.Tensor[Lx,Ly,Lz,3] | None, optional): Vector field correspond to backward transformation. If None, then no transformation is used. Defaults to None. Defaults to None.
+		"""
+		self.dvf_forward = dvf_forward.to(pytomography.device).to(pytomography.dtype)
+		self.dvf_backward = dvf_backward.to(pytomography.device).to(pytomography.dtype)
 		super(DVFMotionTransform, self).__init__()  ## go to the _init_ in Class Transform
   
-	def _get_coordinates(self):  
+	def _get_old_coordinates(self):
+		"""Obtain meshgrid of coordinates corresponding to the object
+
+		Returns:
+			torch.Tensor: Tensor of coordinates corresponding to input object
+		"""
 		dim_x, dim_y, dim_z = self.object_meta.shape
-		coordinates=torch.stack(torch.meshgrid(torch.arange(dim_x),torch.arange(dim_y), torch.arange(dim_z), indexing='ij')).permute((1,2,3,0)).reshape(-1, 3)
+		coordinates=torch.stack(torch.meshgrid(torch.arange(dim_x),torch.arange(dim_y), torch.arange(dim_z), indexing='ij')).permute((1,2,3,0)).to(pytomography.device).to(pytomography.dtype)
 		return coordinates
 
-	def _generate_new_coordinates(self, coordinates, DVF, reverse: bool = False):
-		sign = -1 if reverse else 1 
-		dim_x, dim_y, dim_z = self.object_meta.shape
-		coordinates = coordinates.view(dim_x, dim_y, dim_z, 3)
-		return (coordinates + sign*DVF).flatten(0,2)
+	def _get_new_coordinates(self, old_coordinates: torch.Tensor, DVF: torch.Tensor):
+		"""Obtain the new coordinates of each voxel based on the DVF.
+
+		Args:
+			old_coordinates (torch.Tensor): Old coordinates of each voxel
+			DVF (torch.Tensor): Deformation vector field.
+
+		Returns:
+			_type_: _description_
+		"""
+		dimensions = torch.tensor(self.object_meta.shape).to(pytomography.device)
+		new_coordinates = old_coordinates + DVF
+		new_coordinates = 2/(dimensions-1)*new_coordinates - 1 
+		return new_coordinates
 		
-	def _apply_dvf(self, DVF, object_i):
-		coordinates = self._get_coordinates().cpu()
-		xyz_array = self._generate_new_coordinates(coordinates, DVF)
-		return torch.tensor(map_coordinates(object_i[0].cpu(), xyz_array.T, order=1, mode="nearest")).reshape(self.object_meta.shape).unsqueeze(0).to(pytomography.device)
+	def _apply_dvf(self, DVF: torch.Tensor, object_i: torch.Tensor):
+		"""Applies the deformation vector field to the object
+
+		Args:
+			DVF (torch.Tensor): Deformation vector field
+			object_i (torch.Tensor): Old object.
+
+		Returns:
+			torch.Tensor: Deformed object.
+		"""
+		old_coordinates = self._get_old_coordinates()
+		new_coordinates = self._get_new_coordinates(old_coordinates, DVF)
+		return torch.nn.functional.grid_sample(object_i.unsqueeze(0), new_coordinates.unsqueeze(0).flip(dims=[-1]), align_corners=True)[0]
 
 	def forward( 
 		self,
 		object_i: torch.Tensor,
 	)-> torch.Tensor:
-		return self._apply_dvf(self.dvf_forward, object_i)
+		"""Forward transform of deformation vector field
+
+		Args:
+			object_i (torch.Tensor): Original object.
+
+		Returns:
+			torch.Tensor: Deformed object corresponding to forward transform.
+		"""
+		if self.dvf_forward is None:
+			return object_i
+		else:
+			return self._apply_dvf(self.dvf_forward, object_i)
 	
 	def backward( 
 		self,
 		object_i: torch.Tensor,
 	)-> torch.Tensor:
-		return self._apply_dvf(self.dvf_backward, object_i)
+		"""Backward transform of deformation vector field
+
+		Args:
+			object_i (torch.Tensor): Original object.
+
+		Returns:
+			torch.Tensor: Deformed object corresponding to backward transform.
+		"""
+		if self.dvf_backward is None:
+			return object_i
+		else:
+			return self._apply_dvf(self.dvf_backward, object_i)
