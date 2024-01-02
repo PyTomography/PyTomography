9fcdd43cdf4a0c6eb42f5d8c91ced43a3156560e
Ferrer Ludovic <ludovic.ferrer@ico.unicancer.fr>
2024-01-02 21:59:08 +0100

    remove unused tests directory

--- tests/test_pytomo.py
+++
@@ -1,7 +0,0 @@
-from src.pytomography.io.SPECT import dicom
-
-
-def test_dicom():
-    dcm_file = "tests/jaszczak_disco_c1_j0_fov1.dcm"
-    meta = dicom.get_metadata(dcm_file, index_peak=0)
-    assert meta is not None
